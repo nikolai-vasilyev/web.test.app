b7943b2ac25e665b5b146fe882c4b52960dd3a5a
nikolai-vasilyev <vasiljev.nickolo@yandex.ru>
2025-06-25 12:54:17 +0300

    Test build

--- date.sh
+++ date.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo timedatectl set-timezone Europe/Moscow
-echo "Время сборки: $(date '+%Y-%m-%d %H:%M')" > date_and_v.txt
-echo "$(ls -t .git/refs/tags/ | head -1)" >> date_and_v.txt
+echo "Время начала ковейера: $(date '+%Y-%m-%d %H:%M')" > date_and_v.txt
+echo "$(ls -t .git/refs/tags/ | head -1 | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')" >> date_and_v.txt
